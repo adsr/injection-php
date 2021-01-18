2f368c0d5d4055018567b326f4068574a074736f
Rasmus Lerdorf <rasmus@lerdorf.com>
2021-01-18 08:22:12 -0800

    PHP 8 Support

--- php_injection.h
+++ php_injection.h
@@ -33,6 +33,76 @@ extern zend_module_entry injection_module_entry;
 #    define PHP_INJECTION_API
 #endif
 
+/* backward compat macros */
+
+#if !defined(uint32)
+typedef unsigned int uint32;
+#endif
+
+#if !defined(uint64)
+typedef unsigned long long uint64;
+#endif
+
+#ifndef TSRMLS_CC
+#define TSRMLS_FETCH()
+#define TSRMLS_CC
+#define TSRMLS_DC
+#define TSRMLS_D
+#define TSRMLS_C
+#endif
+
+#ifndef ZVAL_EMPTY_ARRAY
+#define ZVAL_EMPTY_ARRAY(value)         array_init(value)
+#endif
+
+#ifndef RETVAL_EMPTY_ARRAY
+#define RETVAL_EMPTY_ARRAY()            ZVAL_EMPTY_ARRAY(return_value)
+#endif
+
+#ifndef RETURN_EMPTY_ARRAY
+#define RETURN_EMPTY_ARRAY()            do { RETVAL_EMPTY_ARRAY(); return; } while (0)
+#endif
+
+#ifndef IS_MIXED
+# define IS_MIXED 0
+#endif
+
+#ifndef ZEND_ARG_INFO_WITH_DEFAULT_VALUE
+#define ZEND_ARG_INFO_WITH_DEFAULT_VALUE(pass_by_ref, name, default_value) \
+    ZEND_ARG_INFO(pass_by_ref, name)
+#endif
+
+#if PHP_VERSION_ID < 70200
+#undef ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX
+#define ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(name, return_reference, required_num_args, class_name, allow_null) \
+    static const zend_internal_arg_info name[] = { \
+        { (const char*)(zend_uintptr_t)(required_num_args), ( #class_name ), 0, return_reference, allow_null, 0 },
+#endif
+
+#ifndef ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX
+# define ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(name, return_reference, required_num_args, class_name, allow_null) \
+    ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(name, return_reference, required_num_args, class_name, allow_null)
+#endif
+
+#ifndef ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX
+# define ZEND_BEGIN_ARG_WITH_RETURN_TYPE_MASK_EX(name, return_reference, num_args, type) \
+    ZEND_BEGIN_ARG_INFO_EX(name, 0, return_reference, num_args)
+#endif
+
+#ifndef ZEND_BEGIN_ARG_WITH_RETURN_OBJ_TYPE_MASK_EX
+# define ZEND_BEGIN_ARG_WITH_RETURN_OBJ_TYPE_MASK_EX(name, return_reference, required_num_args, class_name, type) \
+    ZEND_BEGIN_ARG_INFO_EX(name, 0, return_reference, required_num_args)
+#endif
+
+#ifndef ZEND_ARG_TYPE_MASK
+# define ZEND_ARG_TYPE_MASK(pass_by_ref, name, type_mask, default_value) \
+    ZEND_ARG_TYPE_INFO(pass_by_ref, name, 0, 0)
+#endif
+
+#ifndef ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE
+# define ZEND_ARG_TYPE_INFO_WITH_DEFAULT_VALUE(pass_by_ref, name, type_hint, allow_null, default_value) \
+    ZEND_ARG_TYPE_INFO(pass_by_ref, name, type_hint, allow_null)
+#endif
 #ifdef ZTS
 #include "TSRM.h"
 #endif
